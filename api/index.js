bdd705885e1b2abc2c4d67ae6c4aa1910e2eff66
prosystem-pro <prosystem490@gmail.com>
2025-02-21 23:23:31 -0600

    ddddd

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
-const app = require('./Src/app');
+const app = require('../src/app');
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+module.exports = app;
